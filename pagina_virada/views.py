3ccf24912c8d2d58d66947794426f25cffe933c6
LukazinDev <lucasdevctt01@gmail.com>
2025-03-19 09:20:24 -0300

    criação da classe InserirPerguntaView

--- pagina_virada/views.py
+++ pagina_virada/views.py
@@ -11,7 +11,7 @@ class MainView (View) :
         lista_ultimas_questoes = Pergunta.objects.oreder_by("-data_criacao")
         contexto = {'perguntas' : lista_ultimas_questoes}
 
-        return render(request, 'projeto/index.html', contexto)
+        return render(request, 'pagina_virada/index.html', contexto)
     
 class PerguntaView (View) :
     def get (self, request, pergunta_id) :
@@ -21,7 +21,7 @@ class PerguntaView (View) :
             raise Http404("Pergunta não encontrada")
         contexto = {'pergunta' : pergunta}
 
-        return render(request, 'projeto/delahte.html', contexto)
+        return render(request, 'pagina_virada/delahte.html', contexto)
     
 class VotoView (View) :
     def get (self, request, resposta_id) :
@@ -37,6 +37,24 @@ class VotoView (View) :
             raise Http404("Pergunta não encontrada")
             resposta.votos += 1
             resposta.save()
-            return redirect(reverse('projeto:detalhe', agrs = [resposta.pergunta]))
+            return redirect(reverse('projeto:detalhe', agrs = [resposta.pergunta.id]))
         
+class InserirPerguntaView (View) :
+    def get (self, request) :
+        return render (request, 'pagina_virada/inserir_pergunta.html')
+    
+    def post (self, request) :
+        if request.user.is_authenticadet :
+            usuario = request.ucer.username
+        else :
+            usuario = "anonimo"
+            titulo = request.POST.get('titulo')
+            detalhe = request.POST.get('detalhe')
+            tentativa = request.POST.get('tentativa')
+            data_criacao = timezone.now()
+            pergunta = Pergunta(titulo = titulo, detalhe = detalhe, tentativa = tentativa, data_criacao = data_criacao, usuario = usuario)
+        
+        pergunta.save()
+        return redirect(reverse('projeto:detalhe', args = [pergunta.id]))
+    
 
