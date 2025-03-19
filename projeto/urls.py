258d12b796c8c0356186ab3f7e01a8e3f4087588
LukazinDev <lucasdevctt01@gmail.com>
2025-03-19 19:37:30 -0300

    Criação do arquivo urls na pasta Projeto_fds

--- projeto/urls.py
+++ projeto/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.contrib import projeto
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+
+    path('pagina_virada/', include{'pagina_virada.ulrs'}),
+    path('projeto/ projeto.site.urls'),
 ]
