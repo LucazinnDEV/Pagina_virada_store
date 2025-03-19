760848237b28cc58497315e0e99d4e29d1bf653f
LukazinDev <lucasdevctt01@gmail.com>
2025-03-19 08:47:40 -0300

    inicialização dos imports necessarios para o arquivo views.py

--- pagina_virada/views.py
+++ pagina_virada/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Pergunta, Resposta
+from django.views import View
+from django.http import HttpResponse
+from django.utils import timezone
+from django.shortcuts import redirect
+from django.urls import reverse 
 
-# Create your views here.
+
